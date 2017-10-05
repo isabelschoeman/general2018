921bd5eb058d872073fb3a12354d3d8d87667ee2
Annabelle Buttenwieser <student@annabelenwieser.dalton.org>
2017-10-05 15:53:30 -0400

    first commit

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Tele2.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by AnnabelleButtenwieser on 9/21/17.
+ */
+//@Disabled
+@TeleOp(name="MN", group="Iterative Opmode")
+public class Tele2 extends OpMode{
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontLeft = null;
+    private DcMotor FrontRight = null;
+    private DcMotor BackLeft = null;
+    private DcMotor BackRight = null;
+    private Servo Servo1;
+    private Servo Servo2;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        FrontLeft  = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackLeft  = hardwareMap.get(DcMotor.class, "BackLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        Servo1 = hardwareMap.get(Servo.class, "Servo1");
+        Servo2 = hardwareMap.get(Servo.class, "Servo2");
+
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.FORWARD);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
+
+
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+
+        // Send calculated power to wheels
+        FrontLeft.setPower(leftPower);
+        BackLeft.setPower(leftPower);
+        FrontRight.setPower(rightPower);
+        BackRight.setPower(rightPower);
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+
+        //Servo Stuff
+
+
+        if (gamepad1.a) {
+            Servo1.setPosition(0.4);
+            Servo2.setPosition(0.55);
+
+        }
+
+        if(gamepad1.b){
+            Servo1.setPosition(0.7);
+            Servo2.setPosition(0.4);
+        }
+
+
+        // strafe gang
+        if (Math.abs(drive) > .2) {
+            telemetry.addData("Status", "Driving");
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            if (drive > 0) {
+                FrontLeft.setPower(leftPower);
+                BackLeft.setPower(-leftPower);
+                FrontRight.setPower(rightPower);
+                BackRight.setPower(-rightPower);
+            } else {
+                FrontLeft.setPower(-leftPower);
+                BackLeft.setPower(leftPower);
+                FrontRight.setPower(-rightPower);
+                BackRight.setPower(rightPower);
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
